5f99aa6b0da2ca760eb1f6c0d5dd4547adb55c09
rodrigo <xravg.rodrigo@gmail.com>
2023-02-22 22:04:22 -0600

    Generalizes URL

--- frontend/src/services/MovieDataService.ts
+++ frontend/src/services/MovieDataService.ts
@@ -1,36 +1,38 @@
 import axios from "axios";
 
+const URL_STRING = "https://movies-revies-app-backend.up.railway.app";
+
 class MovieDataService {
   getAll(page: number = 0) {
-    return axios.get(`https://rravg-movies-reviews-backend.herokuapp.com/api/v1/movies?page=${page}`);
+    return axios.get(`/api/v1/movies?page=${page}`);
   }
 
   get(id: number) {
-    return axios.get(`https://rravg-movies-reviews-backend.herokuapp.com/api/v1/movies/id/${id}`);
+    return axios.get(`${URL_STRING}/api/v1/movies/id/${id}`);
   }
 
   find(query: string, by: string = "title", page: number = 0) {
     return axios.get(
-      `https://rravg-movies-reviews-backend.herokuapp.com/api/v1/movies?${by}=${query}&page=${page}`
+      `${URL_STRING}/api/v1/movies?${by}=${query}&page=${page}`
     );
   }
 
   createReview(data: object) {
-    return axios.post("https://rravg-movies-reviews-backend.herokuapp.com/api/v1/movies/review", data);
+    return axios.post(`${URL_STRING}/api/v1/movies/review`, data);
   }
 
   updateReview(data: object) {
-    return axios.put("https://rravg-movies-reviews-backend.herokuapp.com/api/v1/movies/review", data);
+    return axios.put(`${URL_STRING}/api/v1/movies/review`, data);
   }
 
   deleteReview(id: any, userId: any) {
-    return axios.delete("https://rravg-movies-reviews-backend.herokuapp.com/api/v1/movies/review", {
+    return axios.delete(`${URL_STRING}/api/v1/movies/review`, {
       data: { review_id: id, user_id: userId },
     });
   }
 
   getRatings() {
-    return axios.get("https://rravg-movies-reviews-backend.herokuapp.com/api/v1/movies/ratings");
+    return axios.get(`${URL_STRING}/api/v1/movies/ratings`);
   }
 }
 
